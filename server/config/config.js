bdabb66c95ceb6ebe51bdd7afd728dbd82dccdf6
LKingJ23 <lkingj23@gmail.com>
2019-08-08 13:01:10 +0200

    Google Sing-In

--- server/config/config.js
+++ server/config/config.js
@@ -34,3 +34,8 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//====================
+// Google Client ID
+//====================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '290889031244-i699uu93gao1v8u6j3ps4uthe14nuoqs.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ require('./config/config.js');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-
+const path = require('path');
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Configuracion global de rutas
 app.use(require('./routes/index'));
